96ed46ebb6432e82d5bd4f6bd969fd939f0b9469
Inhwan Kim <ih.kim@vinulabs.com>
2025-01-20 18:39:04 +0900

    Chapter 3

--- 03-asyncsequence-and-intermediate-task/projects/starter/SuperStorage/DownloadView.swift
+++ 03-asyncsequence-and-intermediate-task/projects/starter/SuperStorage/DownloadView.swift
@@ -30,6 +30,7 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import Combine
 import SwiftUI
 import UIKit
 
@@ -44,6 +45,29 @@ struct DownloadView: View {
   @State var isDownloadActive = false
 
   @State var duration = ""
+
+  @State var downloadTask: Task<Void, Error>? {
+    didSet {
+      timerTask?.cancel()
+      guard isDownloadActive else { return }
+
+      let startTime = Date().timeIntervalSince1970
+      let timerSequence = Timer
+        .publish(every: 1, tolerance: 1, on: .main, in: .common)
+        .autoconnect()
+        .map { date -> String in
+          let duration = Int(date.timeIntervalSince1970 - startTime)
+          return "\(duration)s"
+        }
+        .values
+      timerTask = Task {
+        for await duration in timerSequence {
+          self.duration = duration
+        }
+      }
+    }
+  }
+  @State var timerTask: Task<Void, Error>?
 
   var body: some View {
     List {
@@ -64,6 +88,15 @@ struct DownloadView: View {
         },
         downloadWithUpdatesAction: {
           // Download a file with UI progress updates.
+          isDownloadActive = true
+          downloadTask = Task {
+            do {
+              try await SuperStorageModel.$supportsPartialDownloads.withValue(file.name.hasSuffix(".jpeg")) {
+                fileData = try await model.downloadWithProgress(file: file)
+              }
+            } catch { }
+            isDownloadActive = false
+          }
         },
         downloadMultipleAction: {
           // Download a file in multiple concurrent parts.
@@ -88,12 +121,15 @@ struct DownloadView: View {
     .listStyle(.insetGrouped)
     .toolbar {
       Button(action: {
+        model.stopDownloads = true
+        timerTask?.cancel()
       }, label: { Text("Cancel All") })
         .disabled(model.downloads.isEmpty)
     }
     .onDisappear {
       fileData = nil
       model.reset()
+      downloadTask?.cancel()
     }
   }
 }
