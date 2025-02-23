ef783fa548f524059cdaefc74c247659feb7ed4d
Inhwan Kim <a9327370@gmail.com>
2025-02-23 23:04:49 +0900

    Chapter 7.

--- 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
+++ 07-concurrent-code-with-taskgroup/projects/starter/Sky/ScanModel.swift
@@ -60,6 +60,54 @@ class ScanModel: ObservableObject {
 
   func runAllTasks() async throws {
     started = Date()
+    try await withThrowingTaskGroup(of: Result<String, Error>.self) { [unowned self] group in
+      let batchSize = 4
+
+      for index in 0..<batchSize {
+        group.addTask {
+          await self.worker(number: index)
+        }
+      }
+      
+      var index = batchSize
+      
+      for try await result in group {
+        switch result {
+        case .success(let result):
+          print("Completed: \(result)")
+        case .failure(let error):
+          print("Failed: \(error.localizedDescription)")
+        }
+        
+        if index < total {
+          group.addTask { [index] in
+            await self.worker(number: index)
+          }
+          index += 1
+        }
+      }
+      await MainActor.run {
+        completed = 0
+        countPerSecond = 0
+        scheduled = 0
+      }
+      print("Done.")
+    }
+  }
+  
+  func worker(number: Int) async -> Result<String, Error> {
+    await onScheduled()
+    
+    let task = ScanTask(input: number)
+    let result: Result<String, Error>
+    do {
+      result = try .success(await task.run())
+    } catch {
+      result = .failure(error)
+    }
+    
+    await onTaskCompleted()
+    return result
   }
 }
 
