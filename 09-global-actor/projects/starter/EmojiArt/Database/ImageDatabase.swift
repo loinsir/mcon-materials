f307d3f86386e2b1d3d3848459fd6ea504742be8
Inhwan Kim <a9327370@gmail.com>
2025-03-22 22:25:52 +0900

    Chapter 9.

--- 09-global-actor/projects/starter/EmojiArt/Database/ImageDatabase.swift
+++ 09-global-actor/projects/starter/EmojiArt/Database/ImageDatabase.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2023 Kodeco Inc.
+/// Copyright (c) 2025 Razeware LLC
 /// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -30,42 +30,69 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import SwiftUI
+import UIKit
 
-struct DetailsView: View {
-  let file: ImageFile
-  @State var image: UIImage?
-
-  @EnvironmentObject var imageLoader: ImageLoader
-
-  var body: some View {
-    ZStack(alignment: .bottom) {
-      if let image = image {
-        Image(uiImage: image)
-          .resizable()
-          .aspectRatio(contentMode: .fill)
-      } else {
-        Text("No preview available")
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
-          .foregroundColor(.white)
-          .background(Color(.sRGB, white: 0.2, opacity: 1))
-      }
-      VStack(alignment: .center) {
-        Text(file.name)
-          .font(.custom("YoungSerif-Regular", size: 28))
-
-        Text(String(format: "$%.2f", file.price))
-          .font(.custom("YoungSerif-Regular", size: 21))
-          .background(.pink)
-          .foregroundColor(.white)
-      }
-      .padding(.vertical, 40)
-      .clipped()
+@globalActor actor ImageDatabase {
+  static let shared = ImageDatabase()
+  
+  let imageLoader = ImageLoader()
+  
+  private var storage: DiskStorage!
+  private var storedImagesIndex = Set<String>()
+  
+  func setUp() async throws {
+    storage = await DiskStorage()
+    for fileURL in try await storage.persistedFiles() {
+      storedImagesIndex.insert(fileURL.lastPathComponent)
     }
-    .ignoresSafeArea()
-    .foregroundColor(.white)
-    .task {
-      image = try? await ImageDatabase.shared.image(file.url)
+    await imageLoader.setUp()
+  }
+  
+  func store(image: UIImage, forKey key: String) async throws {
+    guard let data = image.pngData() else {
+      throw "Could not convert image \(key)"
     }
+    let fileName = DiskStorage.fileName(for: key)
+    try await storage.write(data, name: fileName)
+    storedImagesIndex.insert(fileName)
+  }
+  
+  func image(_ key: String) async throws -> UIImage {
+    let keys = await imageLoader.cache.keys
+    if keys.contains(key) {
+      print("Cached in-memory")
+      return try await imageLoader.image(key)
+    }
+    do {
+      let fileName = DiskStorage.fileName(for: key)
+      if !storedImagesIndex.contains(fileName) {
+        throw "Image not persisted"
+      }
+      
+      let data = try await storage.read(name: fileName)
+      guard let image = UIImage(data: data) else {
+        throw "Invalid image data"
+      }
+      
+      print("Cached on disk")
+      
+      await imageLoader.add(image, forKey: key)
+      return image
+    } catch {
+      let image = try await imageLoader.image(key)
+      try await store(image: image, forKey: key)
+      return image
+    }
+  }
+  
+  func clear() async {
+    for name in storedImagesIndex {
+      try? await storage.remove(name: name)
+    }
+    storedImagesIndex.removeAll()
+  }
+  
+  func clearInMemoryAssets() async {
+    await imageLoader.clear()
   }
 }
--- 09-global-actor/projects/starter/EmojiArt/Views/BottomToolbar.swift
+++ 09-global-actor/projects/starter/EmojiArt/Views/BottomToolbar.swift
@@ -47,7 +47,10 @@ struct BottomToolbar: View {
       })
 
       Button(action: {
-        // Clear in-memory cache
+        Task {
+          await ImageDatabase.shared.clearInMemoryAssets()
+          try await model.loadImages()
+        }
       }, label: {
         Image(systemName: "square.stack.3d.up.slash")
       })
@@ -58,5 +61,13 @@ struct BottomToolbar: View {
     }
     .padding(.vertical, 2)
     .padding(.horizontal, 5)
+    .task {
+      guard let memoryAccessSequence = ImageDatabase.shared.imageLoader.inMemoryAccess else {
+        return
+      }
+      for await count in memoryAccessSequence {
+        inMemoryAccessCount = count
+      }
+    }
   }
 }
